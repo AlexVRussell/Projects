baeb7a3c9c673556726f9e48aeeb7e5381540326
Alex Russell <128105099+AlexVRussell@users.noreply.github.com>
2023-12-22 17:16:59 -0400

    Update Snake.py
    *CHANGES MADE TO COVERT THE SNAKE GAME TO A GRID FORMAT*
    
    Initial Position of Snake:
    
    Snake starts at the 5th column and 5th row of the grid.
    Initial Position of Food:
    
    Food is placed at random positions within the grid.
    Snake Movement:
    
    Snake moves in increments of SEGMENT_SIZE pixels in each direction.
    Collision with Food:
    
    After a collision, the food is moved to a new random position within the grid.
    
    Other changes will be discussed in the README file

--- Snake.py
+++ Snake.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 import time
 import random
+
 # Initialize pygame
 pygame.init()
 
@@ -11,28 +12,23 @@ BOARD_HEIGHT = 500
 SEGMENT_SIZE = 25
 
 # Set the initial position of the snake and its direction
-snake_x = 0
-snake_y = 0
-snake_direction = (1, 0)  # 1 = right, -1 = left, 2 = down, -2 = up
+snake_x = 5 * SEGMENT_SIZE
+snake_y = 5 * SEGMENT_SIZE
+snake_direction = (1, 0)
 score = 0
 gameOver = "GAME OVER"
-
+nextLevelPrinted = False
 
 # Create a list to store the segments of the snake's body
-snake_body = []
+snake_body = [pygame.Rect(snake_x, snake_y, SEGMENT_SIZE, SEGMENT_SIZE)]
 snake_length = 5
 
 # Create the game board
 screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
 
-
-# Create the snake's head
-snake_head = pygame.Rect((snake_x, snake_y), (SEGMENT_SIZE, SEGMENT_SIZE))
-snake_body.append(snake_head)
-
 # Create the food
-food = pygame.Rect((random.randint(0, BOARD_WIDTH // SEGMENT_SIZE - 1) * SEGMENT_SIZE,
-                   random.randint(0, BOARD_HEIGHT // SEGMENT_SIZE - 1) * SEGMENT_SIZE),
+food = pygame.Rect((random.randint(0, (BOARD_WIDTH // SEGMENT_SIZE) - 1) * SEGMENT_SIZE,
+                    random.randint(0, (BOARD_HEIGHT // SEGMENT_SIZE) - 1) * SEGMENT_SIZE),
                    (SEGMENT_SIZE, SEGMENT_SIZE))
 
 pygame.display.set_caption('Snake Game')
@@ -41,7 +37,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
+            sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 snake_direction = (0, -1)
@@ -53,24 +49,47 @@ while True:
                 snake_direction = (1, 0)
 
     # Move the snake
-    snake_x += snake_direction[0]
-    snake_y += snake_direction[1]
-    snake_head = pygame.Rect((snake_x, snake_y), (SEGMENT_SIZE, SEGMENT_SIZE))
-
-
+    snake_x += snake_direction[0] * SEGMENT_SIZE
+    snake_y += snake_direction[1] * SEGMENT_SIZE
+    snake_head = pygame.Rect(snake_x, snake_y, SEGMENT_SIZE, SEGMENT_SIZE)
 
     if len(snake_body) > snake_length:
         snake_body.pop(0)
 
-    # Check for collision with food
+    '''
+    This block of code is currently not working, I am trying to implement a feature that ends the game when the 
+    snakes head collides with it's body
+    
+    if snake_head.colliderect(snake_body[-1]):
+        print(gameOver)
+        print("FINAL SCORE:", score)
+        pygame.quit()
+        sys.exit()
+    '''
+
     # Check for collision with food
     if snake_head.colliderect(food):
+
         # Move the food to a new random location
-        food = pygame.Rect((random.randint(0, BOARD_WIDTH // SEGMENT_SIZE - 1) * SEGMENT_SIZE,
-                            random.randint(0, BOARD_HEIGHT // SEGMENT_SIZE - 1) * SEGMENT_SIZE),
-                           (SEGMENT_SIZE, SEGMENT_SIZE))
-        snake_length += 25
+        food.x = random.randint(0, (BOARD_WIDTH // SEGMENT_SIZE) - 1) * SEGMENT_SIZE
+        food.y = random.randint(0, (BOARD_HEIGHT // SEGMENT_SIZE) - 1) * SEGMENT_SIZE
+
+        # Update score in realtime in the console for the player to see
+        snake_length += 1
         score += 1
+        print("Score", score, "\n")
+
+    # Update the speed of the snake, when certain score level is reached (Only 2 levels for experimentation)
+    if score >= 5:
+        if score == 5 and not nextLevelPrinted:
+            print("NEXT LEVEL\nSPEED INCREASED\n")
+            nextLevelPrinted = True
+            time.sleep(0.15)
+    if score >= 20:
+        if score == 20 and not nextLevelPrinted:
+            print("NEXT LEVEL\nSPEED INCREASED\n")
+            nextLevelPrinted = True
+            time.sleep(0.17)
 
         # Add a new segment to the snake's body
         snake_body.append(snake_head)
@@ -78,24 +97,18 @@ while True:
         snake_body.append(snake_head)
         if len(snake_body) > snake_length:
             snake_body.pop(0)
-    # Move the snake
-    snake_x += snake_direction[0]
-    snake_y += snake_direction[1]
-
-
 
     # Check if the snake has hit the edge of the board
     if snake_x < 0 or snake_x >= BOARD_WIDTH or snake_y < 0 or snake_y >= BOARD_HEIGHT:
         print(gameOver)
-        print("Score:", score)
+        print("FINAL SCORE:", score)
         pygame.quit()
         sys.exit()
 
-    snake_body.append(snake_head)
-
     # Clear the screen
     screen.fill((0, 0, 0))
-    time.sleep(0.01)
+    if score < 5:
+        time.sleep(0.2)
 
     # Draw the snake and food
     for segment in snake_body:
@@ -104,4 +117,3 @@ while True:
 
     # Update the display
     pygame.display.update()
-
